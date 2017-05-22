4cf1c3f974d95e7c549d217c11ea63a1e733931c
Abdullah Barrak <abdullah@abarrak.com>
2017-05-22 20:17:36 +0300

    rename TestTests to Tests

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestTests")]
+[assembly: AssemblyTitle("MoyasarTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestTests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("MoyasarTests")]
+[assembly: AssemblyCopyright("Copyright ©  2016 - 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test.Tests
+namespace Moyasar.Tests
 {
     [TestClass()]
     public class Class1Tests
