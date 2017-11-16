6294396ea33d641b5292ee137375c3344736fbe1
abarrak <abdullah@abarrak.com>
2017-11-16 14:57:23 +0300

    move MetaResult to Common and add InvoiceListResult

---
+++ moyasar/Invoices/InvoiceListResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Moyasar.Invoices
+{
+    public class InvoiceListResult
+    {
+        public List<InvoiceResult> Invoices { get; set; }
+        public MetaResult Meta { get; set; }
+    }
+}
