eb87a9c56026a3d3e5de298b8b7da52a11b7152a
Ali Alhoshaiyan <alialhoshaiyan@gmail.com>
2019-03-21 23:27:09 +0300

    Fixed cli and unit tests

--- MoyasarEasyCli/PaymentProcessor.cs
+++ MoyasarEasyCli/PaymentProcessor.cs
@@ -18,7 +18,7 @@ namespace MoyasarEasyCli
         {
             Program.ClearPrintOutWelcomeDetails();
             Console.WriteLine();
-            Console.WriteLine("[1] Create Payment");
+            Console.WriteLine("[1] Create Payment [Not Available]");
             Console.WriteLine("[2] Fetch Payment");
             Console.WriteLine("[3] List Payments");
             Console.WriteLine("[0] Go Back");
@@ -49,7 +49,7 @@ namespace MoyasarEasyCli
                 case "0":
                     return true;
                 case "1":
-                    CreatePayment();
+//                    CreatePayment();
                     break;
                 case "2":
                     FetchPayment();
@@ -176,177 +176,6 @@ namespace MoyasarEasyCli
                         PrintOutPayment(payments.Items[int.Parse(choice) - 1]);
                     }
                 }
-            }
-        }
-
-        private static void CreatePayment()
-        {
-            Program.ClearPrintOutWelcomeDetails();
-            Console.WriteLine();
-
-            int amount; 
-            while (true)
-            {
-                Console.Write("Amount: ");
-                try
-                {
-                    amount = int.Parse(Console.ReadLine());
-                    break;
-                }
-                catch
-                {
-                    Console.WriteLine("Invalid value!");
-                }
-            }
-
-            Console.Write("Currency [default=SAR]: ");
-            var currency = Console.ReadLine();
-            if (string.IsNullOrEmpty(currency)) currency = "SAR";
-
-            Console.Write("Description [optional]: ");
-            var desc = Console.ReadLine();
-            if (string.IsNullOrEmpty(desc)) desc = null;
-
-            var source = GetPaymentSource();
-            
-            Console.Write("Callback URL: ");
-            var callback = Console.ReadLine();
-            
-            Payment payment;
-            try
-            {
-                payment = Payment.Create(new PaymentInfo()
-                {
-                    Amount = amount,
-                    Currency = currency,
-                    Description = desc,
-                    Source = source,
-                    CallbackUrl = callback
-                });
-            }
-            catch (ValidationException e)
-            {
-                Console.WriteLine("Incorrect Data:");
-                e.FieldErrors.ForEach(err =>
-                {
-                    Console.WriteLine($"\t- {err.Field}: {err.Error}");
-                });
-                
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
-                return;
-            }
-            catch (NetworkException)
-            {
-                Console.WriteLine("Could not connect to Internet");
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
-                return;
-            }
-            catch (ApiException e)
-            {
-                Console.WriteLine($"Api Exception:\n{e}");
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
-                return;
-            }
-
-            PrintOutPayment(payment);
-        }
-
-        private static IPaymentSource GetPaymentSource()
-        {   
-            var choice = 1;
-            while (true)
-            {
-                try
-                {
-                    Console.Write("Credit Card [1 default] or Sadad [2]: ");
-                    var val = Console.ReadLine();
-                    if (string.IsNullOrEmpty(val)) break;
-                    
-                    choice = int.Parse(val);
-                    if (choice == 1 || choice == 2) break;
-                    
-                    Console.WriteLine("Invalid choice");
-                }
-                catch
-                {
-                    Console.WriteLine("Ex: Invalid choice");
-                }
-            }
-
-            if (choice == 1)
-            {
-                Console.Write("Name: ");
-                var name = Console.ReadLine();
-                
-                Console.Write("Number: ");
-                var number = Console.ReadLine();
-                
-                int cvc; 
-                while (true)
-                {
-                    Console.Write("Cvc: ");
-                    try
-                    {
-                        cvc = int.Parse(Console.ReadLine());
-                        break;
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Invalid Value!");
-                    }
-                }
-                
-                int year; 
-                while (true)
-                {
-                    Console.Write("Year: ");
-                    try
-                    {
-                        year = int.Parse(Console.ReadLine());
-                        break;
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Invalid Value!");
-                    }
-                }
-                
-                int month; 
-                while (true)
-                {
-                    Console.Write("Month: ");
-                    try
-                    {
-                        month = int.Parse(Console.ReadLine());
-                        break;
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Invalid Value!");
-                    }
-                }
-
-                return new CreditCardSource
-                {
-                    Name = name,
-                    Number = number,
-                    Cvc = cvc,
-                    Year = year,
-                    Month = month
-                };
-            }
-            else
-            {
-                Console.WriteLine("Username: ");
-                var username = Console.ReadLine();
-                
-                return new SadadSource
-                {
-                    UserName = username
-                };
             }
         }
         
